706ea6a4e2a6dc48f98e4afd4c0b434dbc31e567
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-10-20 15:15:35 +0000

    [Finish]users#showページまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,9 +2,21 @@ class BooksController < ApplicationController
   def show
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
   def index
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
   end
 
   def index
