7f916c2d5eb39ea966ff32efcb8d7be9776048df
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2023-10-21 15:02:59 +0000

    [Finish]user#showまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -8,6 +9,19 @@ class BooksController < ApplicationController
     @book.save
     redirect_to book_path(@book)
   end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
   def index
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_one_attached :profile_image
 
+
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
